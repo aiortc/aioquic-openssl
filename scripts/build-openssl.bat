ad4a4fd2a6bb58c83e2708c4fab3e45200d4552d
Jeremy Lainé <jeremy.laine@m4x.org>
2024-01-10 23:06:27 +0100

    Update OpenSSL to the latest stable version (3.2.0)
    We also add the following flags to the build:
    
    - no-apps to avoid building the openssl CLI
    - no-dso to avoid building support for dynamic engines

--- scripts/build-openssl.bat
+++ scripts/build-openssl.bat
@@ -21,11 +21,11 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Buil
 SET PATH=%PATH%;C:\Program Files\NASM
 
 mkdir openssl
-curl -L https://www.openssl.org/source/openssl-3.1.4.tar.gz -o openssl.tar.gz
+curl -L https://www.openssl.org/source/openssl-3.2.0.tar.gz -o openssl.tar.gz
 tar xzf openssl.tar.gz -C openssl --strip-components 1
 cd openssl
 
-perl Configure no-comp no-shared no-tests %OPENSSL_CONFIG%
+perl Configure no-apps no-comp no-dso no-shared no-tests %OPENSSL_CONFIG%
 nmake
 
 mkdir %destdir%
